fdf67b241db1e295c708287da350f34a77b7881d
Alejandro-RECO <revolledocoronado@hotmail.com>
2023-07-31 15:15:42 -0500

    Inicalizamos primer curso de React
    Primera fase del curso de React

---
+++ Alura-ORG/src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>HELLO WORD AND REACT</h1>
+    </>
+  )
+}
+
+export default App
